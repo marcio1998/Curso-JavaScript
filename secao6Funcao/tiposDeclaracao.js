9fd250fe8457f9ba8396b10a94f2b376709ce67e
marcio1998 <marciogabriel1998@gmail.com>
2021-08-19 14:43:14 -0300

    Seção 6 Completa

---
+++ secao6Funcao/tiposDeclaracao.js
@@ -0,0 +1,15 @@
+console.log(soma(3, 4))
+
+//function declaration
+function soma(x, y){
+    return x + y
+}
+
+//function expression - funções function express, só pode ser chamada depois da declaração.
+const sub = function(x ,y) {return x - y}
+
+//named function expression
+const mult = function mult(x, y){
+    return x * y
+}
+
