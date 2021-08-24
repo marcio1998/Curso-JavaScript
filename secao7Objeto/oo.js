22c7b5ee35af7f1662a538ab075a1460f28fe141
marcio1998 <marciogabriel1998@gmail.com>
2021-08-24 18:52:14 -0300

    Section 7 parte 1

---
+++ secao7Objeto/oo.js
@@ -0,0 +1,22 @@
+//Código Não executável.
+
+//Procedural.
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+   valor1,
+   valor2,
+   valor3,
+   processamento(){
+       //....
+   } 
+}
+
+objeto.processamento()
+
+//Principios Importantes;
+// 1 - Abstração.
+// 2 - encapsulamento.
+// 3 - herança.
+// 4 - polimorfismo.
