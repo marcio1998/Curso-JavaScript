a6b1ee813e481e216b56faaab284a9326272f5a9
marcio1998 <marciogabriel1998@gmail.com>
2021-09-14 18:54:57 -0300

    Node parte 2

---
+++ secao10Node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,8 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.bemVindo)
+
+const http = require('http')
+http.createServer((req, res)=>{
+    res.write('Bom dia!')
+    res.end()
+}).listen(8080)
